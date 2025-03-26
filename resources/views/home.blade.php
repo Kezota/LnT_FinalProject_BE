42af323bab9703b39b9e1600fdd6729ea6e15d8d
Kezota <keziameilany2005@gmail.com>
2025-03-26 20:20:58 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('title', "PT ChipiChapa")
+
+@section('content')
+    <section class="container container-fluid">
+        <h1>PT ChipiChapa</h1>
+        <p>Welcome to PT ChipiChapa's Website for Final Project LNT BE</p>
+        <p>Created by Kezia :)</p>
+    </section>
+@endsection
